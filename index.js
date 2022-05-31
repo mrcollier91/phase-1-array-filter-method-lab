cb8aac4f6517980cd69cb07a28f0b16a653835cd
mrcollier91 <colliermatt91@gmail.com>
2022-05-31 09:49:24 -0500

    commit tree 4c44ab222022c380dec98fc08a4bf5a5158d0c93

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+/*function findMatching(drivers, name) {
+    let newArray = []
+    for (const string of drivers) {
+        if (string.toLowerCase() === name.toLowerCase()) {
+            newArray.push(string)
+        }
+     }
+     return newArray
+}
+*/
+
+/*
+function findMatching(drivers, name) {
+   let newArray = drivers.filter(function(string){
+        return string.toLowerCase() === name.toLowerCase()
+    })
+    return newArray
+}
+*/
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+    
+/*
+function fuzzyMatch(drivers, name) {
+    let result = drivers.filter(driversName => driversName.startsWith(name))
+    return result
+}
+*/
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.slice(0, name.length) === name)
+}
+   
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+}
